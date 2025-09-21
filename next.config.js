083d976facee21990e3057046950b2b6ce09eb9f
Jjmcclain <joshmcclain1989@gmail.com>
2025-09-21 16:39:19 -0600

    deploy change

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Uncomment these for GitHub Pages
-  ...(process.env.NODE_ENV === 'production' && {
-    basePath: '/player-stats-dashboard',
-    assetPrefix: '/player-stats-dashboard/',
-  }),
+  // // Uncomment these for GitHub Pages
+  // ...(process.env.NODE_ENV === 'production' && {
+  //   basePath: '/player-stats-dashboard',
+  //   assetPrefix: '/player-stats-dashboard/',
+  // }),
 }
 
 module.exports = nextConfig
