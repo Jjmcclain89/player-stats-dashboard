cf43966c0761dae8936a89ce86a6042ece3de86d
Josh McClain <joshmcclain1989@gmail.com>
2025-11-22 17:59:59 -0700

    trim wording

--- src/components/StatsTable/RecordRow.tsx
+++ src/components/StatsTable/RecordRow.tsx
@@ -6,28 +6,32 @@ interface RecordRowProps {
   record2?: { label: string; value: string };
 }
 
+const formatLabel = (label: string) => {
+  return label.replace("Record", "");
+}
+
 const RecordRow: React.FC<RecordRowProps> = ({ record1, record2 }) => {
   return (
     <tr className='flex flex-col lg:flex-row lg:table-row'>
-      <td className='px-3 py-2 border-b text-left font-medium text-sm force-black-text w-full lg:w-1/4'>
-        {record1.label}
+      <td className='px-3 py-2 border-b text-left font-medium text-sm force-black-text w-full lg:w-1/4 border-r'>
+        {formatLabel(record1.label)}
       </td>
-      <td className='px-3 py-2 border-b text-left text-sm force-black-text w-full lg:w-1/4'>
+      <td className='px-3 py-2 border-b text-left text-sm force-black-text w-full lg:w-1/4 border-r'>
         {record1.value}
       </td>
       {record2 ? (
         <>
-          <td className='px-3 py-2 border-b text-left font-medium text-sm force-black-text w-full lg:w-1/4'>
-            {record2.label}
+          <td className='px-3 py-2 border-b text-left font-medium text-sm force-black-text w-full lg:w-1/4 border-r'>
+            {formatLabel(record2.label)}
           </td>
-          <td className='px-3 py-2 border-b text-left text-sm force-black-text w-full lg:w-1/4'>
+          <td className='px-3 py-2 border-b text-left text-sm force-black-text w-full lg:w-1/4 border-r'>
             {record2.value}
           </td>
         </>
       ) : (
         <>
-          <td className='hidden lg:table-cell px-3 py-2 border-b force-black-text lg:w-1/4'></td>
-          <td className='hidden lg:table-cell px-3 py-2 border-b force-black-text lg:w-1/4'></td>
+          <td className='hidden lg:table-cell px-3 py-2 border-b force-black-text lg:w-1/4 border-r'></td>
+          <td className='hidden lg:table-cell px-3 py-2 border-b force-black-text lg:w-1/4 border-r'></td>
         </>
       )}
     </tr>
